5378650b06720c5d0072e11c422baec20e9a1653
kalyan.kafle <kalyan.kafle@aegishms.com>
2025-08-16 19:25:03 +0545

    fixed cors policy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://green-area-frontend.vercel.app",
     credentials: true,
   })
 );
